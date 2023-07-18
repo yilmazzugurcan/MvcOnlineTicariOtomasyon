1f2eda750dd0d7431b965734e67a6a811e403483
Uğurcan Yılmaz <yilmazzugurcan@gmail.com>
2023-07-19 01:43:48 +0300

    4 adet yeni sınıf eklendi

---
+++ MvcOnlineTicariOtomasyon/Models/Class/Cariler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Class
+{
+    public class Cariler
+    {
+        [Key]
+        public int CariID { get; set; }
+        public string CariAd { get; set; }
+        public string CariSoyad { get; set; }
+        public string CariSehir { get; set; }
+        public string CariMail { get; set; }
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Class/Urun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Class
+{
+    public class Urun
+    {
+
+        [Key]
+        public int Urunid { get; set; }
+        public string UrunAd { get; set; }
+        public string Marka { get; set; }
+        
+        public short Stok { get; set; }
+        public decimal AlisFiyat { get; set; }
+        public decimal SatisFiyat { get; set; }
+        public bool Durum { get; set; }
+        public string UrunGorsel { get; set; }
+    }
+}
